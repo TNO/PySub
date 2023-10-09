a7af1accfd060ae2376469f7306a8239e6636ed3
Bart Davids <116873900+davidsbtno@users.noreply.github.com>
2023-10-09 13:54:25 +0200

    Update example scripts to format

--- Example_scripts/Calculate subsidence - Salt moving rigid basement.py
+++ Example_scripts/Calculate subsidence - Salt moving rigid basement.py
@@ -1,27 +1,34 @@
-# -*- coding: utf-8 -*-
-"""Pilot and example script for determining subsidence due to volume change in caverns.
+"""Example and example script for determining subsidence due to volume change in caverns.
 """
 from PySub.memory import build_cavern_model
 from PySub.SubsidenceSuite import ModelSuite
 from PySub import plot_utils as plot
 
-LINE = ((214000,573250),(220000,573800))
-if __name__ == '__main__':
+LINE = ((214000, 573250), (220000, 573800))
+if __name__ == "__main__":
     import_path_salt = r"Input example salt.xlsx"
     import_path_MRB = r"Input example salt MRB.xlsx"
-    
-    salt_model = build_cavern_model(import_path_salt, name = 'withouth MRB',
-                                               project_folder = None)
-    MRB_model = build_cavern_model(import_path_MRB, name = 'with MRB',
-                                               project_folder = None)   
+
+    salt_model = build_cavern_model(
+        import_path_salt, name="withouth MRB", project_folder=None
+    )
+    MRB_model = build_cavern_model(
+        import_path_MRB, name="with MRB", project_folder=None
+    )
     salt_model.calculate_subsidence()
     plot.plot_subsidence(salt_model)
-       
+
     MRB_model.calculate_subsidence()
     plot.plot_subsidence(MRB_model)
-    
-    Suite = ModelSuite('compare salt models', None)
+
+    Suite = ModelSuite("compare salt models", None)
     Suite.set_models((salt_model, MRB_model))
-    
-    plot.plot_cross_section(Suite, lines = LINE, time = -1, y_axis_exageration_factor= 1, figsize = (12,12))
-    plot.plot_timeseries(Suite, mode = 'max')
+
+    plot.plot_cross_section(
+        Suite,
+        lines=LINE,
+        time=-1,
+        y_axis_exageration_factor=1,
+        figsize=(12, 12),
+    )
+    plot.plot_timeseries(Suite, mode="max")
--- Example_scripts/Calculate subsidence.py
+++ Example_scripts/Calculate subsidence.py
@@ -1,51 +1,50 @@
-"""Pilot script for modeling workflow for subsidence"""
+"""Example script for modeling workflow for subsidence. Compares available
+compaction methods."""
+
 #%% import packages used in this example
 from PySub import plot_utils as plot
 from PySub.memory import build_model
 from PySub.SubsidenceSuite import ModelSuite
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     models = []
-    
+
     #%% Make the linear model
     import_path = "Input example linear.json"
-    name = 'linear'
+    name = "linear"
     project_folder = None
-    Model = build_model(import_path, name = name, 
-                        project_folder = project_folder)
-    
+    Model = build_model(import_path, name=name, project_folder=project_folder)
+
     Model.calculate_compaction()
     Model.calculate_subsidence()
-    
+
     # plot with a different background map
     plot.plot_subsidence(Model)
     models.append(Model)
-    
+
     #%% Make the time-decay model
     import_path = "Input example time-decay.xlsx"
-    name = 'time-decay'
-    Model = build_model(import_path, name = name, 
-                        project_folder = project_folder)
-    
+    name = "time-decay"
+    Model = build_model(import_path, name=name, project_folder=project_folder)
+
     Model.calculate_compaction()
     Model.calculate_subsidence()
-    
+
     plot.plot_subsidence(Model)
     models.append(Model)
-    
+
     #%% Make the ratetype model
     import_path = "Input example ratetype.xlsx"
-    name = 'ratetype'
-    Model = build_model(import_path, name = name, 
-                        project_folder = project_folder)
-    
+    name = "ratetype"
+    Model = build_model(import_path, name=name, project_folder=project_folder)
+
     Model.calculate_compaction()
     Model.calculate_subsidence()
-    
+
     plot.plot_subsidence(Model)
     models.append(Model)
-    
-    Suite = ModelSuite("compare", project_folder = project_folder)
+
+    Suite = ModelSuite("compare", project_folder=project_folder)
     Suite.set_models(models)
-    plot.plot_timeseries(Suite, mode = "max")
+    plot.plot_timeseries(Suite, mode="max")
